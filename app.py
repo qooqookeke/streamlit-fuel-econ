c7ec954d4a823c2e665476d01fa42b3fc355c030
qooqookeke <151480658+qooqookeke@users.noreply.github.com>
2023-11-24 10:45:43 +0900

    add

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 def main():
-    st.title('자동차 데이터 분석!')
+    st.title('자동차 데이터 분석!!')
     st.subheader('차트 보기')
     df = pd.read_csv('./data/fuel_econ.csv')
     if st.checkbox('데이터 프레임 보기'): # 체크박스가 체크 되었을때
